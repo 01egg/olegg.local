d9c9a909da4d6106f91d0960a42192e16ba5266a
Griguar <grundickdj@mail.ru>
2023-08-09 20:00:25 +0300

    form

--- blocks/wi-feedback-conf.php
+++ blocks/wi-feedback-conf.php
@@ -1,8 +1,8 @@
 <?php
 
-$mail_addr          = 'to@test.by';
-$mail_topic         = 'Поступила заявка с сайта test.by';
-$mail_from          = 'from@test.by';
+$mail_addr          = 'grundickDJ@mail.ru';
+$mail_topic         = 'Поступила заявка с сайта olegg.top';
+$mail_from          = '01egg@bk.ru';
 $mail_text          = '';
 $recaptcha_secret   = '';
 
